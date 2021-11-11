f32edfe4700c505c9b43f6490e1943c674d57287
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-11-11 13:39:55 +0700

    Update button view loading
    Tạo thêm nút Loading.
    
    khi nào user click vào button thì hiển thị Loading layout

--- AnimationSoundLab31/app/src/main/java/com/example/loadingLab36/M000ActSplash.java
+++ AnimationSoundLab31/app/src/main/java/com/example/loadingLab36/M000ActSplash.java
@@ -2,13 +2,33 @@ package com.example.loadingLab36;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class M000ActSplash extends AppCompatActivity {
+
+    Button btnLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.m001_act_splash);
+
+        View v=findViewById(R.id.incLoadingLayout);
+        btnLoading = findViewById(R.id.btnLoading);
+
+        v.setVisibility(View.GONE);
+
+        btnLoading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.btnLoading){
+                    v.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
